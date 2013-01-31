99f77a4b52945ea1eb7763c162503707303fd093
Martin Zych <martinz425@gmail.com>
2013-01-30 23:20:52 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinbacon::Application.config.secret_token = 'a4258ea0aa870f8ac3693d9d15a0345ae1879fb6b8c91b5f8fa770569045768a2e9982ed7d36a8a506bc151f3bf24c6b748edf2a0d9d5ec626a6d61ca2a4174d'
